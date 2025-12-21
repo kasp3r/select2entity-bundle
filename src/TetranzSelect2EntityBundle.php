8a6ec7d7803d331c3e575d73d719a211ecc1b9c8
Wakonda <amatukami@hotmail.fr>
2025-12-21 07:33:24 +0100

    Update TetranzSelect2EntityBundle.php

--- src/TetranzSelect2EntityBundle.php
+++ src/TetranzSelect2EntityBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class TetranzSelect2EntityBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
